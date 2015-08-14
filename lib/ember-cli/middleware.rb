932e44def31406c0cab38ea45d13533ece940f98
Matt Boldt <me@mattboldt.com>
2015-08-14 15:21:42 -0500

    fixing merge conflict syntax error

--- lib/ember-cli/middleware.rb
+++ lib/ember-cli/middleware.rb
@@ -6,20 +6,7 @@ module EmberCLI
 
     def call(env)
       path = env["PATH_INFO"].to_s
-      @app.call(env)
-    end
 
-    private
-
-    def enable_ember_cli
-      @enabled ||= begin
-        if Rails.env.development? || Rails.env.production_local?
-          EmberCLI.run!
-        else
-          EmberCLI.compile!
-        end
-
-        true
       if path == "/testem.js"
         [ 200, { "Content-Type" => "text/javascript" }, [""] ]
       else
@@ -29,3 +16,40 @@ module EmberCLI
     end
   end
 end
+
+# module EmberCLI
+#   class Middleware
+#     def initialize(app)
+#       @app = app
+#     end
+
+#     def call(env)
+#       path = env["PATH_INFO"].to_s
+
+# <<<<<<< HEAD
+#       @app.call(env)
+#     end
+
+#     private
+
+#     def enable_ember_cli
+#       @enabled ||= begin
+#         if Rails.env.development? || Rails.env.production_local?
+#           EmberCLI.run!
+#         else
+#           EmberCLI.compile!
+#         end
+
+#         true
+# =======
+#         if path == "/testem.js"
+#           [ 200, { "Content-Type" => "text/javascript" }, [""] ]
+#         else
+#           EmberCLI.process_path path
+#           @app.call(env)
+#         end
+# >>>>>>> master
+#       end
+#     end
+#   end
+# end
